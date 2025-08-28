546a2be467232d6a23e2a086d061a1e129686521
Stefanus Chrismanata Candra <stefanus.candra@sawitpro.com>
2025-08-28 10:48:23 +0700

    validate minimum satellite used

--- app/src/main/java/com/template/apptemplate/ui/screens/dummy/GNSSHelper.kt
+++ app/src/main/java/com/template/apptemplate/ui/screens/dummy/GNSSHelper.kt
@@ -8,6 +8,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
+import android.os.Looper
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -21,27 +22,27 @@ data class GNSSData(
 object GNSS {
     private var appContext: Context? = null
     private var locationManager: LocationManager? = null
-
-    // Use WeakReference for callback so Activity/Fragment can be GC'd
     private var callbackRef: WeakReference<(GNSSData) -> Unit>? = null
-
     private var usedSatellites: Int = 0
 
     private val gnssCallback = object : GnssStatus.Callback() {
         override fun onSatelliteStatusChanged(status: GnssStatus) {
             usedSatellites = (0 until status.satelliteCount).count { status.usedInFix(it) }
-            Log.d("GNSS", "Satellites used: $usedSatellites")
-            callbackRef?.get()?.invoke(GNSSData(null, usedSatellites))
+            if (usedSatellites > 5) {
+                callbackRef?.get()?.invoke(GNSSData(null, usedSatellites))
+            } else {
+                Log.d("GNSS", "Satellites used: $usedSatellites")
+            }
         }
     }
 
     private val locationListener = LocationListener { location ->
         if (location.accuracy <= 10) {
-            println("GNSS listener: ${location.latitude}, ${location.longitude}, acc=${location.accuracy}")
+            Log.d("GNSS", "accurate: ${location.latitude}, ${location.longitude}, acc=${location.accuracy}")
+            callbackRef?.get()?.invoke(GNSSData(location, usedSatellites))
         } else {
-            println("GNSS listener, acc=${location.accuracy}")
+            Log.d("GNSS", "inaccurate: ${location.latitude}, ${location.longitude}, acc=${location.accuracy}")
         }
-        callbackRef?.get()?.invoke(GNSSData(location, usedSatellites))
     }
 
     fun init(ctx: Context, callback: (GNSSData) -> Unit) {
@@ -78,7 +79,8 @@ object GNSS {
             LocationManager.GPS_PROVIDER,
             1000L,
             0f,
-            locationListener
+            locationListener,
+            Looper.getMainLooper()
         )
     }
 
